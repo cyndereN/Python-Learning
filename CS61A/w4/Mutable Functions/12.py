9aecf57cb42c92708a34db014401178402acb70e
cyndereN <50982416+cyndereN@users.noreply.github.com>
2022-08-08 13:46:35 +0800

    Update w4 1

---
+++ CS61A/w4/Mutable Functions/12.py
@@ -0,0 +1,42 @@
+def make_withdraw(balance):
+    """Return a withdraw function with a starting balance."""
+    def withdraw(amount):
+        nonlocal balance
+        if amount > balance:
+            return 'Insufficient funds'
+        balance = balance - amount
+        return balance
+    return withdraw
+
+def make_withdraw_list(balance):
+    b = [balance]
+    def withdraw(amount):
+        if amount > b[0]:
+            return 'Insufficient funds'
+        b[0] = b[0] - amount
+        return b[0]
+    return withdraw
+
+# violate referential transparency
+def f(x):
+    x = 4
+    def g(y):
+        def h(z):
+            nonlocal x
+            x = x + 1
+            return x + y + z
+        return h
+    return g
+a = f(1)
+b = a(2)
+b(3) + b(4)  # 10 + 12 = 22
+
+def oski(bear):
+    def cal(berk):
+        nonlocal bear
+        if bear(berk) == 0:
+            return [berk+1, berk-1]
+        bear = lambda ley: berk-ley
+        return [berk, cal(berk)]
+    return cal(2)
+oski(abs)  [2, 3, 1]
