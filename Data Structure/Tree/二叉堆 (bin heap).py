c379308ce35b185dfca8c39ad21309056c5f668f
cyndereN <50982416+cyndereN@users.noreply.github.com>
2023-03-09 12:39:43 +0000

    [Mod] Update Heap

--- Data Structure/Tree/二叉堆 (bin heap).py
+++ Data Structure/Tree/二叉堆 (bin heap).py
@@ -1,4 +1,4 @@
-class BinHeap:
+class BinHeap: #最小堆
     def __init__(self):
         self.heapList = [0] #0占位 根节点从1开始
         self.currentSize = 0
@@ -11,6 +11,7 @@ class BinHeap:
                 self.heaplist[i//2]=self.heapList[i]
                 self.heapList[i]=tmp
             i=i//2 #沿路径向上
+
     def insert(self,k):
         self.heapList.append(k)#添加到末尾
         self.currentSize = self.currentSize + 1
@@ -50,3 +51,63 @@ class BinHeap:
             self.percDown(i)
             i=i-1
         print(self.heapList,i)
+
+
+    
+    # 因为是最小堆，由大到小排序。不断重建小顶堆O(nlogn)
+    def heapSortReturnNewArr(self):
+        # 存到一个新数组中
+        arr = self.heapList[1:]
+        for j in range(self.currentSize-1, 0, -1):
+            arr[0], arr[j] = arr[j], arr[0]
+            
+            # 初始位置
+            i = 0
+            # 左节点的位置
+            p = 2 * i + 1 
+            while (p <= j-1):
+            # 如果右节点存在
+                if p + 1 <= j-1:
+                    # 选出左右节点中 较小的与父节点比较
+                    p = p if arr[p] < arr[p + 1] else p + 1
+                # 当父节点较大时 继续进行交换
+                if arr[i] > arr[p]:
+                    arr[i], arr[p] = arr[p], arr[i]
+                else:  # 否则 退出循环
+                    break
+                i = p
+                p = 2 * i + 1
+
+        print(arr)
+        print(self.heapList)
+        
+    # 原地排序，不需要额外的空间。O(nlogn)
+    # 堆排序本质还是选择排序。适合排序序列个数多的情况(建堆所需比较次数较多)
+    def heapSort(self):
+        for j in range(self.currentSize , 1, -1):
+            self.heapList[1], self.heapList[j] = self.heapList[j], self.heapList[1]
+            self.heapify(1, j-1)
+
+        print(self.heapList)
+        
+
+    def heapify(self, i, heapSize):
+         # 左节点的位置
+        p = 2 * i 
+        while (p <= heapSize):
+            # 如果右节点存在
+            if p + 1 <= heapSize:
+                # 选出左右节点中 较小的与父节点比较
+                p = p if self.heapList[p] < self.heapList[p + 1] else p + 1
+            # 当父节点较大时 继续进行交换
+            if self.heapList[i] > self.heapList[p]:
+                self.heapList[i], self.heapList[p] = self.heapList[p], self.heapList[i]
+            else:  # 否则 退出循环
+                break
+            i = p
+            p = 2 * i 
+
+            
+lis = BinHeap()
+lis.buildHeap([5, 9, 1, 6, 8, 14, 6, 49, 25, 4, 6, 3])
+lis.heapSort()
